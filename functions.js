aa64346248d87ebcbcf0d88a49847aa232f10c33
Appaso Sanjay Avaghadi <appaso1919@gmail.com>
2025-03-27 20:23:42 +0530

    initial update

--- functions.js
+++ functions.js
@@ -1,5 +1,11 @@
-//factorial using function
+//showmessage
+function showMessage() {
+    let message = "Hello!";
+    console.log(message);
+}
+showMessage(); 
 
+//factorial using function
 function factorial(num) {
     let fact = 1;
     for (let i = 1; i <= num; i++) {
@@ -9,3 +15,15 @@ function factorial(num) {
 }
 
 console.log("Factorial of 5 is:", factorial(5));
+
+//Anonymous Function
+const divide = function (a, b) {
+    return a / b;
+};
+ console.log("Divideing :",divide(10, 2)); 
+
+//multiplication unction
+ function multiply(a, b) {
+    return a * b;
+}
+console.log("Multiplication:",multiply(3, 4)); 
